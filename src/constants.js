08dfd64972ac2a5e4f962d42301e930911348129
Maurice Yap <mauriceyap@hotmail.co.uk>
2019-04-06 01:24:02 +0100

    make post path not use localhost

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const WEBSOCKET_ADDRESS = `ws://${window.location.hostname}:2697/ws`;
 export const WEBSOCKET_RECONNECT_INTERVAL = 2000;
 
-export const HTTP_SERVER_BASE_ADDRESS = "http://localhost:2697";
+export const HTTP_SERVER_BASE_ADDRESS = `http://${window.location.hostname}:2697`;
 
 export const INITIAL_NETWORK_TOPOLOGY_LANGUAGE = "YAML";
 
